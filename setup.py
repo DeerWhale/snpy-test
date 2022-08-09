43c3cdeea1d0933a19d37a0e3766a0e9eb9120ec
Jing Lu <lujingeve158@gmail.com>
2022-08-09 18:33:42 -0400

    updated setup.py snpy/kcorr.py and snpy/sn.py to support the new NIR templates

--- setup.py
+++ setup.py
@@ -1,93 +1,29 @@
 #!/usr/bin/env python
 
-# This is the numpy.distutils verison of setup.py.
-
-import os,sys, string
-from pkg_resources import parse_version
-try:
-   import numpy
-except ImportError:
-   print("Error:  setup.py requires numpy")
-   sys.exit(1)
-from numpy.distutils.core import setup
+import sys
 from numpy.distutils.misc_util import Configuration
-from numpy.distutils.system_info import get_info
-try:
-   import matplotlib
-   have_mpl = 1
-except:
-   have_mpl = 0
-
-numpy_min_version = '1.7'
-
-def get_numpy_status():
-   """
-      Returns a dictionary containing a boolean specifying whether NumPy
-      is up-to-date, along with the version string (empty string if
-      not installed).
-   """
-   numpy_status = {}
-   try:
-      import numpy
-      numpy_version = numpy.__version__
-      numpy_status['up_to_date'] = parse_version(
-         numpy_version) >= parse_version(numpy_min_version)
-      numpy_status['version'] = numpy_version
-   except ImportError:
-      numpy_status['up_to_date'] = False
-      numpy_status['version'] = ""
-   return numpy_status
-
 
 def configuration(parent_package='', top_path=None):
-   config = Configuration(None, parent_package, top_path)
-   config.add_subpackage('snpy')
-   config.add_scripts(['bin/snpy'])
-   config.add_scripts(['bin/update-snpy'])
+   config = Configuration('snpy', parent_package, top_path)
+   config.add_subpackage('dm15temp')
+   config.add_subpackage('CSPtemp')
+   config.add_subpackage('filters')
+   config.add_subpackage('spline2')
+   config.add_subpackage('tspack')
+   config.add_subpackage('utils')
+   config.add_subpackage('sqlmod')
+   config.add_subpackage('SwiftTemp')
+   #config.add_scripts(['bin/snpy'])
+   #config.add_scripts(['bin/update-snpy'])
+   config.add_data_dir('typeIa')
+   #config.add_data_files('ipythonrc-SN')
+   config.add_data_files('st_calibration2.dat')
+   config.add_data_files('dm15_calibration2.dat')
+   #config.add_data_files('BmX_Uniform_u_all.pickle')
+   config.add_data_files('color_priors.pickle')
    return config
 
 
-def dosetup():
-   status = get_numpy_status()
-   if not status['up_to_date']:
-      raise ImportError("You need at least version {} to run SNooPy"
-                        .format(numpy_min_version))
-
-
-   with open(os.path.join('snpy','version.py'), 'r') as fi:
-      line = fi.readline()
-      v = line.split('=')[1].strip()
-      version = v[1:-1]
-
-   setup(version=version,
-         name='snpy',
-         description="SNooPy:  Supernova light-curve analysis tool",
-         author='Chris Burns (Carnegie Observatories)',
-         author_email='cburns@carnegiescience.edu',
-         url='http://csp.obs.carnegiescience.edu/data/snpy',
-         license='MIT',
-         classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Environment :: Console',
-            'Framework :: IPython',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Topic :: Scientific/Engineering :: Astronomy'],
-         setup_requires=['numpy','pytest-runner'],
-         tests_require=['pytest'],
-         install_requires=[
-            'numpy',
-            'scipy',
-            'pymysql',
-            'matplotlib',
-            'ipython',
-            'emcee',
-            'pymc',
-            'astropy'],
-         **configuration(top_path='').todict())
-
 if __name__ == '__main__':
-   dosetup()
+   from numpy.distutils.core import setup
+   setup(**configuration(top_path='').todict())
